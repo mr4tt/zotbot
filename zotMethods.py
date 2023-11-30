bfeafe8098c60ca0ea4d611758e883fba3611a42
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-11-29 18:01:12 -0800

    first commit

---
+++ zotMethods.py
@@ -0,0 +1,37 @@
+from pyzotero import zotero
+import os
+
+# TODO: is there any way to get unfiled items? 
+# TODO: sorting?
+
+# get all items from your Zotero library (prints type and ID)
+def getAllItems(zot):
+    items = zot.everything(zot.items())
+    itr = 1
+    for item in items:
+        print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
+        print(itr)
+        itr+= 1
+
+# search by title or author (but i think most things in my lib dont have author)
+def getItemByTitle(zot, search: str, itemType=None):
+    items = zot.everything(zot.items(q=search, itemType=type))
+    itr = 1
+    for item in items:
+        print(item)
+        print('Item: %s | Key: %s | Title: %s | URL: %s' % (item['data']['itemType'], item['data']['key'], item["data"]["title"], item["data"]["url"]))
+        print(itr)
+        itr+= 1
+
+# returns dict of {folder : folder ID}
+def getCollections(zot) -> dict:
+    collection = {}
+    col = zot.collections()
+
+    for folder in col:
+        collection[folder["data"]["name"]] = folder["data"]["key"]
+    
+    return collection
+
+
+
